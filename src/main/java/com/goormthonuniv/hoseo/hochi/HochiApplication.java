ea57ed5581e5917d9353173d3d2b056db99505fe
Jam759 <jam759@naver.com>
2025-04-29 19:24:32 +0900

    init

---
+++ src/main/java/com/goormthonuniv/hoseo/hochi/HochiApplication.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.hoseo.hochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HochiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthonuniv/hoseo/hochi/HochiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.hoseo.hochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HochiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
